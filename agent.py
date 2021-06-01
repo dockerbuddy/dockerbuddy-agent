d084f8e2b3098e025bfb4d73d6b663f060abf244
Frank <bplewnia99@gmail.com>
2021-06-01 11:05:49 +0200

    Handle exceptions and restart thread every 5 sec

--- agent.py
+++ agent.py
@@ -2,7 +2,7 @@ import time
 
 import yaml
 from influxdb_client import InfluxDBClient, Point, WritePrecision
-from influxdb_client.client.write_api import ASYNCHRONOUS
+from influxdb_client.client.write_api import SYNCHRONOUS
 from datetime import datetime
 import psutil
 
@@ -12,11 +12,16 @@ HOME_PATH = '/'
 
 class Agent:
     def __init__(self):
-        self.configuration = self.get_configuration_from_yml()
-        self.influxdb_client = InfluxDBClient(url=self.configuration['INFLUXDB_URL'],
-                                              token=self.configuration['INFLUXDB_TOKEN'])
-        self.influxdb_write_options = ASYNCHRONOUS
-        self.influxdb_data_writer = self.influxdb_client.write_api(self.influxdb_write_options)
+        try:
+            self.configuration = self.get_configuration_from_yml()
+            self.influxdb_client = InfluxDBClient(url=self.configuration['INFLUXDB_URL'],
+                                                  token=self.configuration['INFLUXDB_TOKEN'])
+            self.influxdb_write_options = SYNCHRONOUS
+            self.influxdb_data_writer = self.influxdb_client.write_api(self.influxdb_write_options)
+        except Exception as e:
+            print(f"Configuration failed due to : {e}")
+        else:
+            print("Configuration successful")
 
 
     def get_configuration_from_yml(self):
@@ -24,9 +29,14 @@ class Agent:
             return yaml.load(config, Loader=yaml.BaseLoader)
 
     def write_point_to_influxdb(self, point):
-        self.influxdb_data_writer.write(self.configuration['INFLUXDB_BUCKET'],
-                                        self.configuration['INFLUXDB_ORGANIZATION'],
-                                        point)
+        try:
+            self.influxdb_data_writer.write(self.configuration['INFLUXDB_BUCKET'],
+                                            self.configuration['INFLUXDB_ORGANIZATION'],
+                                            point)
+        except Exception as e:
+            print(f"Writing failed due to : {e}")
+        else:
+            print(f"successful write {point}")
 
     def create_point_from_data(self, data, name):
         return Point(name)\
