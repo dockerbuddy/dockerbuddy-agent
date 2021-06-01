43fb28606224e75f68f1aa9a25fbb58bb95ea8b3
Frank <bplewnia99@gmail.com>
2021-06-01 11:05:49 +0200

    Handle exceptions and restart thread every 5 sec

--- main.py
+++ main.py
@@ -2,13 +2,26 @@
 Agent setup.
 """
 import threading
+import time
 from agent import Agent
 
-if __name__ == "__main__":
+CHECK_AGENT_INTERVAL_TIME = 5
+
+
+def threadwrap():
     agent = Agent()
     threading.Thread(target=agent.run, daemon=True).start()
+
+
+if __name__ == "__main__":
+    agent_thread = threading.Thread(target=threadwrap(), daemon=True)
+    agent_thread.start()
+
     while True:
-        pass
+        time.sleep(CHECK_AGENT_INTERVAL_TIME)
+        if not agent_thread.is_alive():
+            agent_thread = threading.Thread(target=threadwrap(), daemon=True)
+            agent_thread.start()
 
 
 
