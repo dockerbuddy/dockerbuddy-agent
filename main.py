46bcaafade1f5e4ba69c8e684f118c936f48e5b7
Frank <bplewnia99@gmail.com>
2021-06-02 21:42:46 +0200

    FIX duplicate Agent

--- main.py
+++ main.py
@@ -8,19 +8,16 @@ from agent import Agent
 CHECK_AGENT_INTERVAL_TIME = 5
 
 
-def threadwrap():
-    agent = Agent()
-    threading.Thread(target=agent.run, daemon=True).start()
-
-
 if __name__ == "__main__":
-    agent_thread = threading.Thread(target=threadwrap(), daemon=True)
+    agent = Agent()
+    agent_thread = threading.Thread(target=agent.run, daemon=True)
     agent_thread.start()
 
     while True:
         time.sleep(CHECK_AGENT_INTERVAL_TIME)
         if not agent_thread.is_alive():
-            agent_thread = threading.Thread(target=threadwrap(), daemon=True)
+            agent = Agent()
+            agent_thread = threading.Thread(target=agent.run, daemon=True)
             agent_thread.start()
 
 
