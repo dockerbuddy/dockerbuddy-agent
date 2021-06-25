eb1be5d008d5fb987c779e698f14cea2ace95d9e
Frank <bplewnia99@gmail.com>
2021-06-25 20:36:39 +0200

    Fix fields to tags in containers info

--- main.py
+++ main.py
@@ -4,19 +4,18 @@ Agent setup.
 import threading
 import time
 from agent import Agent
-from mock_agent import MockAgent
 
 CHECK_AGENT_INTERVAL_TIME = 5
 
 if __name__ == "__main__":
-    agent = MockAgent()
+    agent = Agent()
     agent_thread = threading.Thread(target=agent.run, daemon=True)
     agent_thread.start()
 
     while True:
         time.sleep(CHECK_AGENT_INTERVAL_TIME)
         if not agent_thread.is_alive():
-            agent = MockAgent()
+            agent = Agent()
             agent_thread = threading.Thread(target=agent.run, daemon=True)
             agent_thread.start()
 
