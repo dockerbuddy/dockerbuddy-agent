b34b77bb21118121c62912b75d0f55072ff7e853
Ilargi12 <bplewnia99@gmail.com>
2021-11-18 21:32:05 +0100

    Add mac-agent package containing mac executable agent

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.9
+FROM python:3.9-alpine
 
 ENV PYTHONFAULTHANDLER=1 \
   PYTHONUNBUFFERED=1 \
@@ -7,14 +7,11 @@ ENV PYTHONFAULTHANDLER=1 \
   PIP_DISABLE_PIP_VERSION_CHECK=on \
   PIP_DEFAULT_TIMEOUT=100
 
-RUN pip install poetry
-
 WORKDIR /app
-COPY poetry.lock pyproject.toml /app/
 
-RUN poetry config virtualenvs.create false
-RUN poetry install --no-interaction --no-ansi --no-root
+RUN pip install pyinstaller
 
 COPY . /app
 
-ENTRYPOINT [ "poetry", "run", "normal" ]
+RUN ls /app
+RUN pyinstaller --onefile /app/dockiera-agent/src/agent/agent.py
--- src/agent/agent.py
+++ src/agent/agent.py
@@ -1,15 +1,116 @@
+import enum
 import os
+import threading
 import time
 from concurrent.futures.thread import ThreadPoolExecutor
-from datetime import datetime
+from dataclasses import dataclass
+from datetime import datetime, timezone
 from typing import Any, List
 
 import docker
 import psutil
+from dataclasses_json import LetterCase, dataclass_json
+from loguru import logger
+from requests.api import post
 
-from .common import get_iso_timestamp, get_metric_from_data, send_summary_to_backend
-from .config import HOME_PATH, MAX_WORKERS
-from .dataclasses import ContainerState, ContainerSummary, HostSummary
+HOME_PATH = "/"
+MAX_WORKERS = 50
+CHECK_IF_AGENT_ALIVE_TIME = 10
+
+
+class ContainerState(enum.Enum):
+    restarting = "RESTARTING"
+    dead = "DEAD"
+    created = "CREATED"
+    exited = "EXITED"
+    paused = "PAUSED"
+    running = "RUNNING"
+    removing = "REMOVING"
+
+
+class MetricType(enum.Enum):
+    memory_usage = "MEMORY_USAGE"
+    disk_usage = "DISK_USAGE"
+    cpu_usage = "CPU_USAGE"
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class BasicMetric:
+    metric_type: MetricType
+    value: float
+    total: float
+    percent: float
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class ContainerSummary:
+    id: str
+    name: str
+    image: str
+    status: str
+    metrics: List[BasicMetric]
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class HostSummary:
+    id: str
+    timestamp: datetime
+    metrics: List[BasicMetric]
+    containers: List[ContainerSummary]
+
+
+def send_summary_to_backend(endpoint: str, data: Any) -> None:
+    try:
+        headers = {"Content-type": "application/json", "Accept": "application/json"}
+        print(data.to_json())
+        response = post(url=endpoint, headers=headers, data=data.to_json())
+        logger.info(f"SENT SUMMARY TO {endpoint}. STATUS CODE: {response.status_code}")
+    except Exception as e:
+        logger.error(f"FAILED TO SEND SUMMARY TO {endpoint}")
+
+
+def get_metric_from_data(metric_name: str, data: Any) -> BasicMetric:
+    if metric_name == "virtual_memory":
+        return BasicMetric(MetricType.memory_usage, data.used, data.total, data.percent)
+    elif metric_name == "disk_memory":
+        return BasicMetric(MetricType.disk_usage, data.used, data.total, data.percent)
+    elif metric_name == "host_cpu_usage":
+        percentage = data
+        return BasicMetric(MetricType.cpu_usage, percentage, 100, percentage)
+    elif metric_name == "container_cpu_usage":
+        # NOTE (@bplewnia) - Divide by number of nanoseconds in second -> 10e9
+        percentage = (
+            abs(
+                data["cpu_stats"]["cpu_usage"]["total_usage"]
+                - data["precpu_stats"]["cpu_usage"]["total_usage"]
+            )
+            * 100
+            / 10 ** 9
+        )
+        return BasicMetric(MetricType.cpu_usage, percentage, 100, percentage)
+    elif metric_name == "container_memory_usage":
+        return (
+            BasicMetric(MetricType.memory_usage, 0, 0, 0)
+            if not data
+            else BasicMetric(
+                MetricType.memory_usage,
+                data["usage"],
+                data["limit"],
+                (data["usage"] / data["limit"]) * 100,
+            )
+        )
+    else:
+        logger.error(f"DID NOT FIND OPTION FOR {metric_name}")
+        return BasicMetric(MetricType.cpu_usage, 0, 0, 0)
+
+
+def get_iso_timestamp() -> str:
+    return (
+        datetime.now(timezone.utc).replace(microsecond=0, tzinfo=None).isoformat() + "Z"
+    )
 
 
 class Agent:
@@ -17,9 +118,11 @@ class Agent:
         self.docker_client = docker.from_env()
         self.executor = ThreadPoolExecutor(max_workers=MAX_WORKERS)
 
-        self.host_id = os.environ["HOST_ID"]
-        self.backend_endpoint = os.environ["BACKEND_ENDPOINT"]
-        self.fetch_freq = int(os.environ["FETCH_FREQ"])
+        self.host_id = os.environ.get("AGENT_HOST_ID", "1")
+        self.backend_endpoint = os.environ.get(
+            "AGENT_BACKEND_ENDPOINT", "http://localhost:8080/api/v2/metrics"
+        )
+        self.fetch_freq = int(os.environ.get("AGENT_FETCH_FREQ", "10"))
 
     def get_containers_summary(self) -> List[ContainerSummary]:
         summaries = []
@@ -66,3 +169,14 @@ class Agent:
             host_summary = self.get_host_summary()
             send_summary_to_backend(endpoint=self.backend_endpoint, data=host_summary)
             time.sleep(self.fetch_freq)
+
+
+if __name__ == "__main__":
+    agent_thread = threading.Thread(target=Agent().run, daemon=True)
+    agent_thread.start()
+
+    while True:
+        time.sleep(CHECK_IF_AGENT_ALIVE_TIME)
+        if not agent_thread.is_alive():
+            agent_thread = threading.Thread(target=Agent().run, daemon=True)
+            agent_thread.start()
