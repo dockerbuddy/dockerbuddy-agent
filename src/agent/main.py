f744beaa1a636a57f8a3e9a01d143b2eae9fa690
Ilargi12 <bplewnia99@gmail.com>
2021-11-28 18:09:43 +0100

    Remove mock_agent execution

--- src/agent/main.py
+++ src/agent/main.py
@@ -3,7 +3,6 @@ import time
 
 from .agent import Agent
 from .config import CHECK_IF_AGENT_ALIVE_TIME
-from .mock_agent import MockAgent
 
 
 def run_normal_agent():
@@ -15,9 +14,3 @@ def run_normal_agent():
         if not agent_thread.is_alive():
             agent_thread = threading.Thread(target=Agent().run, daemon=True)
             agent_thread.start()
-
-
-def run_mock_agent():
-    agent = MockAgent()
-    agent_thread = threading.Thread(target=agent.run, daemon=True)
-    agent_thread.start()
